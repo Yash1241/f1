85e861776ac76d9ef97f8151938daec03c67b8ba
Yash1241 <144287242+Yash1241@users.noreply.github.com>
2025-06-30 00:36:48 -0500

    Update L3Firewall.py

--- L3Firewall.py
+++ L3Firewall.py
@@ -1,92 +1,191 @@
-sudo mn --topo=single,4 --mac --controller=remote,port=6655 --switch=ovsk
-*** Creating network
-*** Adding controller
-*** Adding hosts:
-h1 h2 h3 h4
-*** Adding switches:
-s1
-*** Adding links:
-(h1, s1) (h2, s1) (h3, s1) (h4, s1)
-*** Configuring hosts
-h1 h2 h3 h4
-*** Starting controller
-c0
-*** Starting 1 switches
-s1
-*** Starting CLI:
-mininet>
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.addresses import IPAddr, EthAddr
+from pox.lib.util import dpid_to_str
+import pox.lib.packet as pkt
+from pox.lib.revent import EventMixin
+import os
+import csv
+
+log = core.getLogger()
+
+class Firewall (EventMixin):
+  def __init__ (self, l2config, l3config):
+    self.l2config_file = l2config
+    self.l3config_file = l3config
+
+    self.l2_blocked_mac_pairs = []
+    self.mac_ip_map = {}
+    self.blocked_macs = set()
+
+    core.openflow.addListenerByName("PacketIn", self._handle_PacketIn)
+    core.openflow.addListenerByName("ConnectionUp", self._handle_ConnectionUp)
+
+    log.info("Initializing L3Firewall module...")
+    self.load_l2_firewall_rules()
+    self.load_l3_firewall_rules()
+
+  def load_l2_firewall_rules(self):
+    self.l2_blocked_mac_pairs = []
+    if not os.path.exists(self.l2config_file):
+        log.warning("L2 firewall config file %s not found. No L2 rules loaded." % self.l2config_file)
+        return
+
+    try:
+        with open(self.l2config_file, 'r') as f:
+            reader = csv.reader(f)
+            header = next(reader, None)
+            if header:
+                log.info("L2 firewall config header: %s" % (", ".join(header)))
+
+            log.info("Loading L2 firewall rules from %s" % self.l2config_file)
+            for row in reader:
+                if len(row) >= 2:
+                    source_mac_str = row[1].strip()
+                    dest_mac_str = row[2].strip() if len(row) > 2 else "any"
+
+                    if source_mac_str.lower() != 'any':
+                        try:
+                            source_mac = EthAddr(source_mac_str)
+                            dest_mac = EthAddr(dest_mac_str) if dest_mac_str.lower() != 'any' and dest_mac_str.strip() != '' else None
+                            self.l2_blocked_mac_pairs.append((source_mac, dest_mac))
+                            log.debug("Loaded L2 block rule: src_mac=%s, dst_mac=%s" % (source_mac, dest_mac_str))
+                        except Exception as e:
+                            log.error("Error parsing L2 rule line '%s': %s" % (",".join(row), e))
+                else:
+                    log.warning("Skipping malformed L2 rule line: %s" % (",".join(row)))
+
+    except Exception as e:
+        log.error("Error loading L2 firewall config from %s: %s" % (self.l2config_file, e))
+        self.l2_blocked_mac_pairs = []
+    log.info("Loaded %s L2 firewall rules." % len(self.l2_blocked_mac_pairs))
 
 
-/////////////////////////////////////////////////////
-  ubuntu@ubuntu:~ $
-mininet> h1 ifconfig h1-eth0 192.168.2.10/24
-mininet> h2 ifconfig h2-eth0 192.168.2.20/24
-mininet> h3 ifconfig h3-eth0 192.168.2.30/24
-mininet> h4 ifconfig h4-eth0 192.168.2.40/24
-mininet> h1 hping3 192.168.2.20 -c 1000 -s 0 --flood --rand-source
-hping 192.168.2.20 (lo 192.168.2.20): S set, 40 headers
-HPING 192.168.2.20 (lo 192.168.2.20): S set, 40 headers 0 data bytes
-hping in flood mode, no replies will be shown
-/////////////////////////////////////////////////////////////
-ubuntu@ubuntu:~$ sudo python /home/ubuntu/pox/pox.py openflow.of_01 --port=6655 pox.forwarding.L3Firewall --l2config=/home/ubuntu/pox/pox/forwarding/l2firewall.config --l3config=/home/ubuntu/pox/pox/forwarding/l3firewall.config
-POX 0.5.0 (eel) is up.
-INFO:Initializing L3Firewall module...
-INFO:L2 firewall config header: id,mac_0,mac_1
-INFO:Loading L2 firewall rules from /home/ubuntu/pox/pox/forwarding/l2firewall.config
-INFO:Loaded 0 L2 firewall rules.
-INFO:Attempting to load L3 firewall rules from /home/ubuntu/pox/pox/forwarding/l3firewall.config (currently empty).
-INFO:Switch 00-00-00-00-00-00-00-01 connected. Installing initial firewall rules.
-INFO:Installed low-priority default forwarding rule for learning
-  //////////////////////////////////////////
+  def load_l3_firewall_rules(self):
+      log.info("Attempting to load L3 firewall rules from %s (currently empty)." % self.l3config_file)
+      self.rules = []
+      if not os.path.exists(self.l3config_file):
+          log.warning("L3 firewall config file %s not found. No L3 rules loaded." % self.l3config_file)
+          return
 
-mininet> h3 ping -c 1 192.168.2.40
-PING 192.168.2.40 (192.168.2.40) 56(84) bytes of data.
-64 bytes from 192.168.2.40: icmp_seq=1 ttl=64 time=0.045 ms
+  def install_flow (self, match, drop_packet=False, priority=None, idle_timeout=0, hard_timeout=0):
+    msg = of.ofp_flow_mod()
+    msg.match = match
+    if priority is not None:
+        msg.priority = priority
+    else:
+        msg.priority = 1000
 
---- 192.168.2.40 ping statistics ---
-1 packets transmitted, 1 received, 0% packet loss, time 0ms
-rtt min/avg/max/mdev = 0.045/0.045/0.045/0.000 ms
-mininet>
+    msg.idle_timeout = idle_timeout
+    msg.hard_timeout = hard_timeout
 
-/////////////////////////////////////////
-mininet> h1 hping3 192.168.2.20 -c 10000 -S --flood --rand-source -V
-HPING 192.168.2.20 (h1-eth0 192.168.2.20): S set, 40 headers + 0 data bytes
-hping in flood mode, no replies will be shown
-^C
-////////////////////////////////////////////////////
-  INFO:Port security: Learned MAC 00:00:00:00:00:01 -> IP 192.168.2.10
-WARNING:!!! SPOOFING DETECTED !!! MAC 00:00:00:00:00:01 (original IP: 192.168.2.10) is now using IP 47.244.102.57
-INFO:Blocking future IP traffic from spoofing MAC: 00:00:00:00:00:01
-WARNING:!!! SPOOFING DETECTED !!! MAC 00:00:00:00:00:01 (original IP: 192.168.2.10) is now using IP 143.208.124.56
-INFO:Blocking future IP traffic from spoofing MAC: 00:00:00:00:00:01
-DEBUG:Installing DROP flow for match: dl_src=00:00:00:00:00:01,dl_type=0x800
-////////////////////////////////////////////////
+    if drop_packet:
+        log.debug("Installing DROP flow for match: %s" % (match,))
+    else:
+        log.debug("Installing FORWARD (OFPP_NORMAL) flow for match: %s" % (match,))
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_NORMAL))
 
-mininet> ovs-ofctl dump-flows s1
-NXST_FLOW reply (xid=0x4):
- cookie=0x0, duration=X.YYYs, table=0, n_packets=Z, n_bytes=W, idle_timeout=0, hard_timeout=0, priority=30000,dl_src=00:00:00:00:00:01,dl_type=0x800 actions=drop
- cookie=0x0, duration=A.BBBs, table=0, n_packets=C, n_bytes=D, idle_timeout=10, hard_timeout=30, priority=1000,tcp,tp_dst=80,nw_src=192.168.2.10,nw_dst=192.168.2.20 actions=output:2
- cookie=0x0, duration=E.FFFs, table=0, n_packets=G, n_bytes=H, priority=1 actions=NORMAL
+    if hasattr(self, 'connection') and self.connection:
+        self.connection.send(msg)
+    else:
+        log.error("No active connection to send flow modification for match: %s." % match)
 
-////////////////////////////////////////////
+  def _handle_PacketIn (self, event):
+    packet = event.parsed
+    if not packet.parsed:
+        log.warning("Ignoring unparsed packet")
+        return
+
+    # Yash Patel: Static L2 Firewall Rule Check from l2firewall.config.
+    for source_mac_to_block, dest_mac_if_specific in self.l2_blocked_mac_pairs:
+        if packet.src == source_mac_to_block:
+            if dest_mac_if_specific is None or (packet.dst == dest_mac_if_specific):
+                log.info("Packet from MAC %s blocked by L2 firewall rule (matched: %s->%s)." % (packet.src, source_mac_to_block, dest_mac_if_specific))
+                match = of.ofp_match(dl_src=packet.src)
+                if dest_mac_if_specific:
+                    match.dl_dst = dest_mac_if_specific
+                # Yash Patel: Install drop flow for L2 rule.
+                self.install_flow(match, drop_packet=True, priority=65535, idle_timeout=0, hard_timeout=0)
+                event.halt = True # Yash Patel: Stop processing this packet.
+                return
+
+    # Yash Patel: Port Security Logic (Dynamic Spoofing Detection).
+    ip_packet = packet.find('ipv4')
+    if ip_packet:
+        # Yash Patel: Check if source MAC is already identified as a spoofer.
+        if packet.src in self.blocked_macs:
+            log.debug("Packet from already blocked spoofing MAC %s dropped." % packet.src)
+            event.halt = True # Yash Patel: Stop processing.
+            return
+
+        if packet.src not in self.mac_ip_map:
+            # Yash Patel: New MAC, record its associated IP.
+            self.mac_ip_map[packet.src] = ip_packet.srcip
+            log.info("Port security: Learned MAC %s -> IP %s" % (packet.src, ip_packet.srcip))
+        elif self.mac_ip_map[packet.src] != ip_packet.srcip:
+            # Yash Patel: Port security violation detected!
+            log.warning("!!! SPOOFING DETECTED !!! MAC %s (original IP: %s) is now using IP %s" % (packet.src, self.mac_ip_map[packet.src], ip_packet.srcip))
+            self.blocked_macs.add(packet.src) # Yash Patel: Add MAC to blocked_macs.
+
+            # Yash Patel: Install flow to block all future IP traffic from this spoofing MAC.
+            match = of.ofp_match(dl_src=packet.src, dl_type=pkt.ethernet.IP_TYPE)
+            self.install_flow(match, drop_packet=True, priority=of.OFP_DEFAULT_PRIORITY + 20000, idle_timeout=0, hard_timeout=0)
+            log.info("Blocking future IP traffic from spoofing MAC: %s" % packet.src)
+            event.halt = True # Yash Patel: Stop processing the current spoofed packet.
+            return
+
+    # Yash Patel: Default Learning Switch Behavior (if not blocked).
+    # Yash Patel: Handles ARP and legitimate IP traffic.
+    # Yash Patel: Install a forwarding flow for the matched packet.
+    match = of.ofp_match.from_packet(packet)
+    self.install_flow(match, drop_packet=False, priority=1000, idle_timeout=10, hard_timeout=30)
+    
+    # Yash Patel: Send the current packet out if no flow was found.
+    msg = of.ofp_packet_out(data=packet.pack())
+    msg.actions.append(of.ofp_action_output(port=of.OFPP_NORMAL))
+    event.connection.send(msg)
 
 
-mininet> h1 ping -c 4 192.168.2.20
-PING 192.168.2.20 (192.168.2.20) 56(84) bytes of data.
+  def _handle_ConnectionUp (self, event):
+    self.connection = event.connection
+    log.info("Switch %s connected. Installing initial firewall rules." % dpid_to_str(event.dpid))
 
---- 192.168.2.20 ping statistics ---
-4 packets transmitted, 0 received, 100% packet loss, time 3006ms
-mininet>
+    # Yash Patel: Install initial static L2 block rules (re-applying on connect).
+    for source_mac, dest_mac in self.l2_blocked_mac_pairs:
+        match = of.ofp_match(dl_src=source_mac)
+        if dest_mac:
+            match.dl_dst = dest_mac
+        self.install_flow(match, drop_packet=True, priority=65535, idle_timeout=0, hard_timeout=0)
+        log.info("Installed initial static L2 block flow for MAC %s->%s (on switch %s)" % (source_mac, dest_mac, dpid_to_str(event.dpid)))
 
-////////////////////////////////////////////////////////////////
- h3 ping -c 1 192.168.2.40
-PING 192.168.2.40 (192.168.2.40) 56(84) bytes of data.
-64 bytes from 192.168.2.40: icmp_seq=1 ttl=64 time=0.043 ms
+    # Yash Patel: Re-install dynamic spoofing block rules for previously blocked MACs.
+    for mac in self.blocked_macs:
+        match = of.ofp_match(dl_src=mac, dl_type=pkt.ethernet.IP_TYPE)
+        self.install_flow(match, drop_packet=True, priority=of.OFP_DEFAULT_PRIORITY + 20000, idle_timeout=0, hard_timeout=0)
+        log.info("Re-installed block flow for spoofing MAC %s on switch %s" % (mac, dpid_to_str(event.dpid)))
+        
+    # Yash Patel: Install a low-priority default rule for learning switch behavior.
+    self.install_flow(of.ofp_match(), drop_packet=False, priority=1, idle_timeout=0, hard_timeout=0)
+    log.info("Installed low-priority default forwarding rule for learning.")
 
---- 192.168.2.40 ping statistics ---
-1 packets transmitted, 1 received, 0% packet loss, time 0ms
-rtt min/avg/max/mdev = 0.043/0.043/0.043/0.000 ms
-mininet>
 
-///////////////////////////////////////////////////////////////////////
+def launch (l2config="l2firewall.config", l3config="l3firewall.config"):
+  if not os.path.exists(l2config):
+      try:
+          with open(l2config, 'w') as f:
+              writer = csv.writer(f)
+              writer.writerow(["id", "mac_0", "mac_1"])
+          log.info("Created empty L2 firewall config file: %s" % l2config)
+      except IOError as e:
+          log.error("Could not create L2 config file %s: %s" % (l2config, e))
+    
+  if not os.path.exists(l3config):
+      try:
+          with open(l3config, 'w') as f:
+              writer = csv.writer(f)
+              writer.writerow(["id", "src_ip", "dst_ip", "nw_proto", "src_port", "dst_port", "priority"])
+          log.info("Created empty L3 firewall config file: %s" % l3config)
+      except IOError as e:
+          log.error("Could not create L3 config file %s: %s" % (l3config, e))
 
+  core.registerNew(Firewall, l2config, l3config)
